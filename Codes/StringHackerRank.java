322d75f42fd2a92e544d444cf1d8ffff58d41457
shresth27 <shresthshruti09@gmail.com>
2018-01-18 19:32:27 +0530

    New Java Programs

---
+++ Codes/StringHackerRank.java
@@ -0,0 +1,32 @@
+/* Given two strings of lowercase English letters,  and , perform the following operations:
+
+Sum the lengths of A and B .
+Determine if  A is lexicographically larger than B (i.e.: does A come before B in the dictionary?).
+Capitalize the first letter in  A and B and print them on a single line, separated by a space. */
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        System.out.println(A.length() + B.length());
+        if(A.compareTo(B)>0)
+        {
+            System.out.println("Yes");
+        }
+        else
+        {
+            System.out.println("No");
+        }
+        String C= A.substring(0,1).toUpperCase() + A.substring(1);
+        String D= B.substring(0,1).toUpperCase() + B.substring(1);
+        System.out.println(C + " " + D);
+        
+    }
+}
+
--- log.md
+++ log.md
@@ -11,3 +11,19 @@ Completed all the challenges of CSS and HTML5 on FreeCodeCamp.
 ### Day 2: January 14th, 2018
 
 Installed MongoDB and learned to do basic queries on it.
+
+### Day 3: January 15th, 2018
+
+Started learning Python and did some basic programs on it.
+
+### Day 4: January 16th, 2018
+
+Continued the challenges on Responsive Bootstrap on FreeCodeCamp . Will put a break on it for a couple of days to focus on practising algorithm programs.
+
+### Day 5: January 17th, 2018
+
+Did a couple of programs on Java which cleared a loads of doubts and made programming seem a little less daunting. It is all about practising!
+
+### Day 6: January 18th, 2018
+
+Did some programs on patterns in Java and studied about CRUD in MongoDB.
